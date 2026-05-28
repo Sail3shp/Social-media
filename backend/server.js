3dea91bdf7e37076af1c1c5965f2dc10eacbb0d2
Sail3shp <saileshpokhrel557@gmail.com>
2026-05-28 08:46:41 +0545

    fix: port number

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 import { connectDb } from './config/connectDb.js'
 dotenv.config()
-
 const PORT = process.env.PORT || 9256
 const app = express()
 
-app.listen(8848,() => {
+app.use(cookieParser())
+
+app.listen(PORT,() => {
     connectDb()
     console.log(`server is running in ${PORT}`)
 })
