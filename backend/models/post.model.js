6e091d6807ff988a14d4f023741477a59a1041c6
Sail3shp <saileshpokhrel557@gmail.com>
2026-06-02 08:48:04 +0545

    added post model

---
+++ backend/models/post.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    likes:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    image: {
+        type: String
+    },
+    caption: {
+        type: String,
+        trim: true
+    },
+    comments: [
+        {
+            text: {
+                type: String,
+                required: true,
+            },
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true,
+            }
+        }
+    ]
+},{
+    timestamps: true
+})
+
+const Post = mongoose.Model('Post',postSchema)
+
+export default Post 
