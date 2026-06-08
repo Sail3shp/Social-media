913410cf898536c7c86ea8c45fef79df0668d897
Sail3shp <saileshpokhrel557@gmail.com>
2026-06-08 14:14:40 +0545

    added ui elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,23 @@ import { Route, Routes } from 'react-router'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import HomePage from './pages/HomePage'
+import Sidebar from './component/Sidebar'
+import RightPanel from './component/RightPanel'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
 
   return (
     
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
       <Routes>
         <Route path='/' element = {<HomePage />} />
         <Route path='/login' element = {<LoginPage />} />
         <Route path='/signup' element = {<SignupPage />} />
+        <Route path = '/profile/johndoe' element = { <ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   )
 }
