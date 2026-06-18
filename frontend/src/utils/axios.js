6f775c31c39619e7b92c8f86db6dd19f65a767ac
Sail3shp <saileshpokhrel557@gmail.com>
2026-06-18 08:34:09 +0545

    fix:axios issue

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://social-media-tosd.onrender.com/',
+    baseURL: 'https://social-media-tosd.onrender.com/api/v1',
     withCredentials: true
 })
 
