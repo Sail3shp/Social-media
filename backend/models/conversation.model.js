f27562319cef43f424f5e59ec938c639f65b1e7e
Sail3shp <saileshpokhrel557@gmail.com>
2026-06-22 23:21:22 +0545

    schema:chat Schema

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema({
+    members: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    ],
+    messages: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Message',
+            default: []
+        }
+    ]
+}, { timestamps: true })
